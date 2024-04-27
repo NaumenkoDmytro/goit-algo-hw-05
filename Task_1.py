5fae9162ca3fbbe080828177a1db04e33f74bb77
NaumenkoDmytro <naymenko.dmitry5@gmail.com>
2024-04-27 13:12:12 +0300

    Update

--- Task_1.py
+++ Task_1.py
@@ -4,3 +4,27 @@
 Реалізуйте функцію caching_fibonacci, яка створює та використовує кеш для зберігання і повторного використання вже обчислених значень чисел Фібоначчі.
 
 '''
+
+def caching_fibonacci():
+    cache = {}
+    def fibonacci(n):
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n in cache:
+            return cache[n]
+        else:
+            cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+            return cache[n]
+
+    return fibonacci
+
+# Отримуємо функцію fibonacci
+fib = caching_fibonacci()
+
+# Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
+print(fib(10))  # Виведе 55
+print(fib(15))  # Виведе 610
+
+            
