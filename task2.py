d9a9763976013c3ca14d9b4265054c86e6fb4c42
NaumenkoDmytro <naymenko.dmitry5@gmail.com>
2024-04-28 20:20:19 +0300

    Tasj updated

--- task2.py
+++ task2.py
@@ -10,8 +10,10 @@ def generator_numbers(text: str):
         for number in numbers:
             yield float(number)
 
+
 def sum_profit(text: str, fucn: callable):
     return sum(fucn(text))
+
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 total_income = sum_profit(text,generator_numbers)
